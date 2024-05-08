89ae31e29a33845c0dbf412a1e696fd092a4c839
lesson240 <lesson240@gmail.com>
2024-05-09 02:54:54 +0900

    modifying, fetch02_oliveyoung function

--- 02-scraping.py
+++ 02-scraping.py
@@ -9,6 +9,8 @@
 
 # pip install -U selenium  https://www.selenium.dev/selenium/docs/api/py/index.html
 from selenium import webdriver  # Web browser 상호 작용을 자동화하는데 지원
+from selenium.webdriver.common.by import By   # https://selenium-python.readthedocs.io/locating-elements.html#locating-by-id
+from selenium.webdriver.common.keys import Keys
 
 # pip install asyncio  https://peps.python.org/pep-3156/
 import asyncio  # 비동기 I/O 지원
@@ -22,7 +24,6 @@ import aiohttp  # 비동기 HTTP Client/Server 지원
 # pip install aiodns   https://docs.aiohttp.org/en/stable/glossary.html#term-aiodns
 import aiodns  # 비동기식 DNS 확인을 수행
 
-import time
 
 # selenium 4 에서는 chrome driver auto up-to-date 지원
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -32,8 +33,10 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 
 # pip install pymongo   https://www.w3schools.com/PYTHON/python_mongodb_getstarted.asp
 import pymongo 
-from types import NoneType
 
+from datetime import date # https://docs.python.org/ko/3/library/datetime.html#date-objects
+import math  # https://docs.python.org/ko/3/library/math.html
+import time
 
 
 
@@ -44,35 +47,128 @@ class ScrapingBrowser:
         self.name = name
         self.url = url
 
-    def chrome_webdriver(self):
+    async def fetch02_oliveyoung(self):
         """chrome webdriver 함수"""
-        # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-        service = webdriver.ChromeService()
-        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/\
-            537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
-        # chrome://version 에서 확인
-        options = webdriver.ChromeOptions()
+        # 개발자도구에서 Preferences > Debugger > Disable JavaScript 로 확인
+        async with aiohttp.ClientSession() as driver:
+            # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+            service = webdriver.ChromeService()
+            user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/\
+                537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+            # chrome://version 에서 확인
+            options = webdriver.ChromeOptions()
 
-        # options.add_argument("headless") # no browser
-        options.add_argument("window_size=1920x1080")  # --window-size=x,y
-        options.add_argument("lang=ko_KR")
-        options.add_argument("disable-gpu")  # gpu err 발생시 , --disable-gpu로 변경
-        options.add_argument(f"user-agent={user_agent}")
-        options.add_argument("mute-audio")  # --mute-audio
-        # options.add_argument("inconnito") # secret mode
+            # options.add_argument("headless") # no browser
+            options.add_argument("window_size=1920x1080")  # --window-size=x,y
+            options.add_argument("lang=ko_KR")
+            options.add_argument("disable-gpu")  # gpu err 발생시 , --disable-gpu로 변경
+            options.add_argument(f"user-agent={user_agent}")
+            options.add_argument("mute-audio")  # --mute-audio
+            # options.add_argument("inconnito") # secret mode
 
-        options.add_experimental_option("excludeSwitches", ["enable-logging"])
-        options.add_experimental_option("detach", True)
+            options.add_experimental_option("excludeSwitches", ["enable-logging"])
+            options.add_experimental_option("detach", True)
 
-        browser = webdriver.Chrome(service=service, options=options)
-        browser.maximize_window()
+            driver = webdriver.Chrome(service=service, options=options)
+            driver.maximize_window()
 
-        browser.get(self.url)
-        # print(self.url)
-        # browser.implicitly_wait(10)
-        # browser.quit()
+            driver.get(self.url)
+            # print(self.url)
+            # driver.implicitly_wait(10)
+            # driver.quit()
 
-        # return browser
+            # return driver
+        
+            totCnt = [] # 실제 상품 수
+            goods_totCnt = driver.find_element(By.ID,"totCnt").text
+            totCnt.append(int(goods_totCnt))
+            page_calculation = math.ceil(totCnt[0]/48) # total page 구하는 공식
+            print(page_calculation)
+
+            driver.find_element(By.XPATH,'//*[@id="sub_gbn_cate"]/div[2]/div[2]/ul/li[3]').click()
+            # num = 1
+            # collectiontime = date.today()
+
+            goodsno_elements = driver.find_elements(By.CSS_SELECTOR,"a")
+            price_elements = driver.find_elements(By.CLASS_NAME,"total")
+            for element in goodsno_elements:
+                goodsno = element.get_attribute("data-ref-goodsno")
+                # total_price = price.text
+                # price_elements = driver.find_elements(By.XPATH,'//*[@id="allGoodsList"]/ul[12]/li[3]/div/a')
+                element = []
+                if goodsno == None:
+                    pass
+                elif goodsno != element:
+                    element.append(goodsno) # 동일한 코드 삭제 함수 만들기
+                    # element.update(total_price)
+                # print(total_price)
+                    print(element)
+                #     print(type(element))
+
+            # price_elements = driver.find_elements(By.CLASS_NAME,"total")
+            # for price in price_elements:
+            #     total_price = price.text
+            #     if not total_price: # empty string 제거
+            #         pass
+            #     else:
+            #         print(total_price)
+
+
+
+
+            # price_elements = driver.find_elements(By.CSS_SELECTOR,"div")
+            # for element in price_elements:
+            #     price = element.get_property("price")
+                # element = []
+                # if price == None:
+                #     pass
+                # elif price != element:
+                #     element.append(price) # 동일한 코드 삭제 함수 만들기
+                #     print(element)
+
+                    # href = element.get_attribute("href")
+                # src = element.get_attribute("src")
+                # soldout = element.get_attribute("status_flag soldout")
+
+
+            cc = driver.find_element(By.XPATH,'//*[@id="allGoodsList"]/ul[12]/li[4]/div/a').text
+            dd = driver.find_element(By.XPATH,'//*[@id="allGoodsList"]/ul[12]/li[4]/div/div[1]/a').text
+            ee = driver.find_element(By.XPATH,'//*[@id="allGoodsList"]/ul[12]/li[4]/div/div[1]/div[2]/div[1]').text
+            ff = driver.find_element(By.XPATH,'//*[@id="allGoodsList"]/ul[12]/li[4]/div/div[1]/div[2]/div[2]').text
+            print(cc)
+            print(dd)
+            print(ee)
+            print(ff)
+            # for goods in goodslist:
+            #     driver.find_element()
+
+                # simple_pdtdic = {"number":f"{num}","code":f"{code}","brand":f"{name}","discount":f"{discount}",\
+                #             "price":f"{price}","soldout":f"{soldout}","time":f"{collectiontime}","staus":True}
+
+
+
+            # if totCnt[0] < 24:
+            #     pass
+
+
+                # 상품 스크래핑 기능
+
+            # elif totCnt[0] > 36:
+                # 1. 자바스크립트를 이용한 방법
+                # element = driver.find_element(By.XPATH,"//*[@id='sub_gbn_cate']/div[2]/div[2]/ul/li[3]")
+                # driver.execute_script("arguments[0].click()",element)
+                # 2. 자바스크립트 문법 & html id 이용
+                # driver.execute_script("document.querySelector('#btnSearch').click()")
+                
+
+                # for page in range(1,page_calculation):
+                    # 상품 스크래핑 기능
+                    
+                    # if page_claculation이 1 초과할 경우, next page로의 click 이동
+
+
+
+            # driver.close()
 
 
     def oliveyoung(self):
@@ -92,7 +188,8 @@ class ScrapingBrowser:
                 for info in area_info:
                     code = info["data-ref-onlbrndcd"]
                     name = info.text
-                    branddic = {"number":f"{num}","code":f"{code}","brand":f"{name}","staus":True}
+                    collectiontime = date.today()
+                    branddic = {"number":f"{num}","code":f"{code}","brand":f"{name}","time":f"{collectiontime}","staus":True}
                     num = num + 1
                     # myclient, mydb 접속
                     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -103,18 +200,24 @@ class ScrapingBrowser:
                     mycol.update_one({"code":f"{code}"}, {"$set":branddic}, upsert=True)                          
                     # print(x.inserted_id)
 
-            await session.close() # 43초 기록
+            await session.close() # 43초 기록 # logging module 필요
        
 
+
+
+
+
     def oliveyoungbrandlist(self):
-        """oliveyoung brand list scraping function"""
+        """Function for scraping the brand list of the oliveyoung"""
         asyncio.run(ScrapingBrowser.fetch01_oliveyoung(self))
 
+    def oliveyoungbrandshop(self):
+        """Function for scraping the detail page of the oliveryoung brand shop"""
+        asyncio.run(ScrapingBrowser.fetch02_oliveyoung(self))
 
 
 
-
-
+# front input values
 
 browser_db = ["https://www.oliveyoung.co.kr"]
 oliveyoung_brandlist = {"아벤느": "A000003"}
@@ -126,7 +229,7 @@ oliveyoung_page_brandlist_arg = ScrapingBrowser(
 
 oliveyoung_page_brandshopdetail_arg = ScrapingBrowser(
     "올리브영",
-    f"{browser_db[0]}/display/getBrandShopDetail.do?onlBrndCd={oliveyoung_brandlist['아벤느']}",
+    f"{browser_db[0]}/store/display/getBrandShopDetail.do?onlBrndCd={oliveyoung_brandlist['아벤느']}",
 )
 oliveyoung_page_goodsdetail_arg = ScrapingBrowser(
     "올리브영",
@@ -145,7 +248,8 @@ oliveyoung_page_goodsdetail = f"{browser_db[0]}/store/goods/getGoodsDetail.do?go
 if __name__ == "__main__":
     start = time.time()
     # asyncio.run(main())
-    ScrapingBrowser.oliveyoungbrandlist(oliveyoung_page_brandlist_arg)
+    # ScrapingBrowser.oliveyoungbrandlist(oliveyoung_page_brandlist_arg)
+    ScrapingBrowser.oliveyoungbrandshop(oliveyoung_page_brandshopdetail_arg)
     # ManagingDatabase()
     end = time.time()
     print(end - start)
---
+++ functest.py
@@ -0,0 +1,19 @@
+import datetime 
+import time
+from datetime import date
+import math
+
+totCnt = [36]
+maxCnt = 48
+
+calculation = totCnt[0]/maxCnt
+
+print(math.ceil(calculation))
+# if __name__=="__main__":
+
+#     bb = datetime.datetime.now()
+#     cc = date.today()
+
+#     print(bb, cc)
+
+
